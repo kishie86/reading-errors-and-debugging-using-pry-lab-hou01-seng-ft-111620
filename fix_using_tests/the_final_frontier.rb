a6d3bfef7f4100a1604e5ecb3a56e61051bc5e9c
Kishana Liburd <kishana.liburd@gmail.com>
2020-10-16 00:56:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -10,7 +10,7 @@ def state_log(star_date)
   "Captain's Log, star date #{star_date}."
 end
 
-def engage(date)
+def engage(state_log)
   date = generate_star_date
   #binding.pry
   puts state_log(date)
